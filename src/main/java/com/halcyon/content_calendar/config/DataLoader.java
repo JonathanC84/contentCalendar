8c712f8ea1623f755337f518b08358ddeae33654
JonathanC84 <jonathan.cayrol@gmail.com>
2024-08-25 17:30:11 +0200

    Prod

--- src/main/java/com/halcyon/content_calendar/config/DataLoader.java
+++ src/main/java/com/halcyon/content_calendar/config/DataLoader.java
@@ -1,12 +1,31 @@
 package com.halcyon.content_calendar.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.halcyon.content_calendar.model.Content;
+import com.halcyon.content_calendar.repository.ContentRepository;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+import java.io.InputStream;
+import java.util.List;
+
+@Component
 public class DataLoader implements CommandLineRunner {
+
+    private final ContentRepository repository;
+    private ObjectMapper objectMapper;
+
+    public DataLoader(ContentRepository repository, ObjectMapper objectMapper) {
+        this.repository = repository;
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+            repository.saveAll(objectMapper.readValue(inputStream,new TypeReference<List<Content>>(){}));
+        }
     }
 }
